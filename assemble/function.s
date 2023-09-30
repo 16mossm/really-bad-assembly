3c4302062cfac6ef109229e4628fc05a2bb4e36e
16mossm <50373533+16mossm@users.noreply.github.com>
2023-09-30 19:43:14 +0100

    fixed function.s
    also removed nfuction as it did not work

--- assemble/function.s
+++ assemble/function.s
@@ -2,69 +2,62 @@
 ## its basically a function but in assembly
 
 .global _start
-_start:
-    subl $8, %esp
-    movl $3, 4(%esp)### repacement for pushl 
-    movl $2, 8(%esp)###    
-    #push $3        #pass variable 1
-    #push $2        #pass variable 2
-    call power
-    addl $8, %esp   #delete the two variables from the stack (8 = 4 * 2)
-    
-    movl %eax , %ebx ## move the returned value
+_start:  
+    push $2        #pass variable 2
+    push $5        #pass variable 1
 
-    movl $1, %eax #exit (%ebx is returned)
+    ## so this will be 5 to the power of 2
+    call power
+
+
+    add $16, %rsp   #delete the two variables from the stack (16 =  * 2)
+    mov %rax , %rbx ## move the returned value
+
+
+    movq $1, %rax #exit (%rbx is returned)
     int $0x80           ##magic man do his things
 
 
 
 
 #purpose:
-# does A to the power of B 
-# preamiters: 
-##int a 
+# does A to the power of B
+# preamiters:
+##int a
 ##int b
 #output:
 #returns the A to the power of B
 
 
-power:
-    subl $4, %ebp
-    movl %ebp , 4(%esp)##  repacement for pushl 
-    ####push %ebp           ##save the old base pointer
-    mov %esp, %ebp     ##make the stack pointer the base pointer   ##ebp is variables ##esp is top
-    subl $8, %esp
+power:      #########logic is broken
+    push %rbp           ##save the old base pointer
+    movq %rsp, %rbp     ##make the stack pointer the base pointer   ##ebp is variables ##esp is top
+    sub $8, %rsp        ##add 1 local variable to the stack
 
-    ####    %eax - base
-    ####    %ebx - power
-    ##      %ecx - temporary value
+    ####    %rax - base
+    ####    %rbx - power
+    ##      %rcx - temporary total
 
-    ##   -4(%ebp) - current value
+    movq 16(%rbp),  %rax     #get the variables passed
+    movq 24(%rbp),  %rbx
 
-
-    movl 8(%ebp),  %eax     #get the variables passed
-    movl 12(%ebp),  %ebx
-
-    movl $1,-4(%ebp)        #if power is 0 then reuslt is 1 because math
-    cmpl $0, %ebx          #if power = 0 then break
-    jle power_end_loop
-    movl %eax, -4(%ebp)     #store the currnet base
-
+    movq $1, %rcx       #if power is 0 then end value will be 1
     power_loop_start:
-        decl %ebx       #subtract power by 1
-        cmpl $0, %ebx       #end when 0
-        jle power_end_loop
-        movl -4(%ebp) , %ecx
-        imull %eax, %ecx    #multiply the currnet answet by the base
-        movl  %ecx, -4(%ebp)
-        jmp power_loop_start    ##end_loop      
+        cmp $0, %rbx        #if power hits 0 end
+        je power_end_loop  
+        imul %rax, %rcx         #multiply total by the base
+        dec %rbx                #decrease power by 1
+        jmp power_loop_start
 
     power_end_loop:
 
-        movl -4(%ebp), %eax # result goes into eax to be returned
-        movl %ebp, %esp     #delete local varaibles by sliding the stack
-        addl $4 , %ebp  #replace for popl
-        #popl %ebp       #restore the base pointer
+        #mov $5, %rbx
+        #imul %rbx, %rax
+
+
+        movq %rcx, %rax # result goes into rax to be returned
+
+        movq %rbp, %rsp     #delete all local varaibles by sliding the stack
+
+        pop %rbp            #restore the base pointer
         ret
-
-
